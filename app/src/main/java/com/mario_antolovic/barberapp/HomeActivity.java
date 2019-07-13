194f8681ecf6608344f4f8d5ade8c754b64bc16d
zanerage <marioantolovic0@gmail.com>
2019-07-13 20:55:50 +0200

    update user / firebase database connection / preloaded fonts fragments / home and shopping

--- app/src/main/java/com/mario_antolovic/barberapp/HomeActivity.java
+++ app/src/main/java/com/mario_antolovic/barberapp/HomeActivity.java
@@ -1,13 +1,166 @@
 package com.mario_antolovic.barberapp;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.BottomSheetDialog;
+import android.support.design.widget.TextInputEditText;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.facebook.accountkit.Account;
+import com.facebook.accountkit.AccountKit;
+import com.facebook.accountkit.AccountKitCallback;
+import com.facebook.accountkit.AccountKitError;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.mario_antolovic.barberapp.Common.Common;
+import com.mario_antolovic.barberapp.Fragments.HomeFragment;
+import com.mario_antolovic.barberapp.Fragments.ShoppingFragment;
+import com.mario_antolovic.barberapp.Model.User;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class HomeActivity extends AppCompatActivity {
+
+    @BindView(R.id.bottom_navigation)
+    BottomNavigationView bottomNavigationView;
+
+    BottomSheetDialog bottomSheetDialog;
+
+    CollectionReference userRef;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        ButterKnife.bind(HomeActivity.this);
+
+
+        //init
+        userRef = FirebaseFirestore.getInstance().collection("User");
+
+        // check intent  , if is login = true , enable full access
+        // if is login false , just let user around shopping to view
+        if (getIntent()!= null)
+        {
+            boolean isLogin = getIntent().getBooleanExtra(Common.IS_LOGIN,false);
+            if(isLogin) {
+                // check if user is exist
+                AccountKit.getCurrentAccount(new AccountKitCallback<Account>() {
+                    @Override
+                    public void onSuccess(final Account account) {
+                        if (account != null)
+                        {
+                            DocumentReference currentUser = userRef.document(account.getPhoneNumber().toString());
+                            currentUser.get()
+                                    .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                            if (task.isSuccessful())
+                                            {
+                                                DocumentSnapshot userSnapShot = task.getResult();
+                                                if (!userSnapShot.exists())
+                                                    showUpdateDialg(account.getPhoneNumber().toString());
+                                            }
+                                        }
+                                    });
+                        }
+                    }
+
+                    @Override
+                    public void onError(AccountKitError accountKitError) {
+                        Toast.makeText(HomeActivity.this, ""+accountKitError.getErrorType().getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        }
+
+        // view
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            Fragment fragment = null;
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+             if (menuItem.getItemId()== R.id.action_home)
+                 fragment = new HomeFragment();
+             else if (menuItem.getItemId()==R.id.action_shopping)
+                 fragment = new ShoppingFragment();
+
+             return loadFragment(fragment);
+
+
+            }
+        });
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null)
+        {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,fragment)
+                    .commit();
+            return true;
+
+        }
+        return false;
+    }
+
+    private void showUpdateDialg(final String phoneNumber) {
+// init dialog
+        bottomSheetDialog = new BottomSheetDialog(this);
+        bottomSheetDialog.setCanceledOnTouchOutside(false);
+        bottomSheetDialog.setCancelable(false);
+        View sheetView = getLayoutInflater().inflate(R.layout.layout_update_user,null);
+
+        Button update_btn = (Button)sheetView.findViewById(R.id.update_btn);
+        final TextInputEditText edt_name = (TextInputEditText)sheetView.findViewById(R.id.edt_name);
+        final TextInputEditText edt_address = (TextInputEditText)sheetView.findViewById(R.id.edt_address);
+
+
+        update_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                User user = new User(edt_name.getText().toString(),
+                        edt_address.getText().toString(),
+                        phoneNumber);
+                userRef.document(phoneNumber)
+                        .set(user)
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                bottomSheetDialog.dismiss();
+                                Toast.makeText(HomeActivity.this, "Thank you", Toast.LENGTH_SHORT).show();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        bottomSheetDialog.dismiss();
+                        Toast.makeText(HomeActivity.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+            }
+        });
+
+
+        bottomSheetDialog.setContentView(sheetView);
+
+        bottomSheetDialog.show();
+
+
+
+
     }
 }
---
+++ app/src/main/java/com/mario_antolovic/barberapp/Model/User.java
@@ -0,0 +1,24 @@
+package com.mario_antolovic.barberapp.Model;
+
+public class User {
+
+    private String name,address,phoneNumber;
+
+    public User() {
+
+    }
+
+    public User(String name,String address,String phoneNumber) {
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+
+    }
+    public String getName() {
+        return name;
+
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
